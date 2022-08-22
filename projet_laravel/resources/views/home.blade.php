9b95d38f21036997c15244761658f2af8cfb3bb2
Ess_Weber <aessotchossim@gmail.com>
2022-08-22 14:34:14 +0200

    modif on home page

---
+++ projet_laravel/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Mon app laravel</title>
+
+
+    </head>
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+          <h1>hollo every from Togo !!!!</h1>
+          <p>current time : {{date('d/m/y h:i A')}} .</p>
+          <footer>&copy; copyright {{date('Y')}} &middot; <a href="#">About as</a></footer>
+    </body>
+   
+</html>
