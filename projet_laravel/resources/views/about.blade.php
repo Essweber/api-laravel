b051191413c25bd196ca6ae2b02431b56e7c22d2
Ess_Weber <aessotchossim@gmail.com>
2022-08-22 14:53:34 +0200

    add of about us page

--- projet_laravel/resources/views/about.blade.php
+++ projet_laravel/resources/views/about.blade.php
@@ -4,14 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Mon app laravel</title>
+        <title>qui some nous</title>
 
 
     </head>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-          <h1>hollo every from Togo !!!!</h1>
-          <p>current time : {{date('d/m/y h:i A')}} .</p>
-          <footer>&copy; copyright {{date('Y')}} &middot; <a href="/about-us">About as</a></footer>
+          <h1>A propos de nous</h1>
+          <p>Buld with &hearts; by Gibert fullstack !!!</p>
+            <p><a href="/">revenir à la page d'accueil</a></p>
+
+          <footer>&copy; copyright {{date('Y')}} &middot; <a href="#">About as</a></footer>
     </body>
    
 </html>
